643c6137b91351b2ba1d0fdeb2a5b60c978e1e94
Azaki Fathoni Hantoro <160385978+ryJQ@users.noreply.github.com>
2025-06-17 17:29:55 +0700

    update efficiennetB0

--- testing model/inference_test.py
+++ testing model/inference_test.py
@@ -1,38 +1,41 @@
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+from tensorflow.keras.applications.efficientnet import preprocess_input
 
 # Path model dan gambar
 MODEL_PATH = r"D:\\KULIAH UNDIP\\SKRIPSI S NYA SANTUY\\python\\GuavaDisease_CNN_Model.tflite"
-IMAGE_PATH = r"D:\KULIAH UNDIP\SKRIPSI S NYA SANTUY\DATASET\GuavaDiseaseDataset\test\Anthracnose\29_unsharp_clahe_augmented_7.png"
+IMAGE_PATH = r"D:\KULIAH UNDIP\SKRIPSI S NYA SANTUY\DATASET\GuavaDiseaseDataset\test\fruit_fly\22_unsharp_clahe_augmented_7.png"
 
-# Daftar label sesuai urutan pelatihan
-CLASS_NAMES = ['Anthracnose', 'Fruit Flies', 'Healthy']
+# Harus cocok dengan urutan class_names saat training
+CLASS_NAMES = ['anthracnose', 'fruit_fly', 'healthy_guava']
 
 # Load TFLite model
 interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
 interpreter.allocate_tensors()
 
-# Dapatkan info input/output
+# Get input/output info
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Load dan preprocess gambar
 img = Image.open(IMAGE_PATH).convert('RGB').resize((224, 224))
-img_array = np.array(img).astype(np.float32) / 255.0  # Normalisasi 0–1
-img_array = np.expand_dims(img_array, axis=0)  # (1, 224, 224, 3)
+img_array = np.array(img).astype(np.float32)
+img_array = preprocess_input(img_array)  # PENTING untuk EfficientNet
+img_array = np.expand_dims(img_array, axis=0)
 
-# Set input ke model
+# Set input
 interpreter.set_tensor(input_details[0]['index'], img_array)
 
 # Inference
 interpreter.invoke()
 
-# Ambil hasil output
+# Get output
 output = interpreter.get_tensor(output_details[0]['index'])
 predicted_index = np.argmax(output)
 confidence = output[0][predicted_index]
 
-# Print hasil
+# Output
 print(f"Prediksi: {CLASS_NAMES[predicted_index]}")
 print(f"Akurasi: {confidence * 100:.2f}%")
+print(f"Output raw: {output}")
